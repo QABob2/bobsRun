cd4c9bc8aacbc2d92a3cff4e58a308a92eb1d214
rosmall <rosmall@AGPF38543G.allegisgroup.com>
2022-06-29 07:51:26 -0700

    first test case

---
+++ Object Repository/PageTitle.rs
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>PageTitle</name>
+   <tag></tag>
+   <elementGuidId>dcaa724f-8fce-40d4-92d4-152073a472f8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>head > title</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>css</name>
+      <type>Main</type>
+      <value>head > title</value>
+      <webElementGuid>1c09dbd3-4cf1-4401-abcd-5f8d30467a0c</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
