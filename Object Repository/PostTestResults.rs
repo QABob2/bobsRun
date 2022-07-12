efb0f940ce4ec34a43eee6904e8dd248792b128c
jriostek <jriosolmedo@teksystems.com>
2022-07-12 14:26:50 -0500

    added event listener to update results of test

--- Object Repository/PostTestResults.rs
+++ Object Repository/PostTestResults.rs
@@ -9,12 +9,24 @@
    <connectionTimeout>-1</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
-   <httpBodyContent></httpBodyContent>
-   <httpBodyType></httpBodyType>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n  \&quot;event\&quot;:{\&quot;Environment\&quot;:\&quot;${GlobalVariable.Environment.value}\&quot;, \&quot;Application\&quot;:\&quot;${GlobalVariable.Application.value}\&quot;, \&quot;TestType\&quot;:\&quot;${GlobalVariable.TestType.value}\&quot;, \&quot;Feature\&quot;:\&quot;${GlobalVariable.Feature.value}\&quot;, \&quot;TestResult\&quot;:\&quot;${GlobalVariable.TestResult.value}\&quot;, \&quot;TestTechnique\&quot;:\&quot;${GlobalVariable.TestTechnique.value}\&quot;, \&quot;TestName\&quot;:\&quot;${GlobalVariable.TestName.value}\&quot;}\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>01cfcb9e-f33f-4e7d-a6e3-0540c21c60c3</webElementGuid>
+   </httpHeaderProperties>
    <katalonVersion>8.3.5</katalonVersion>
    <maxResponseSize>-1</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
-   <restRequestMethod>GET</restRequestMethod>
+   <restRequestMethod>POST</restRequestMethod>
    <restUrl></restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
@@ -24,5 +36,18 @@
    <soapServiceFunction></soapServiceFunction>
    <socketTimeout>-1</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
